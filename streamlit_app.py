84e78e3b7af158a5ca10e04a8c718370ac5834c2
Thaha1899 <110521719+Thaha1899@users.noreply.github.com>
2022-10-10 16:08:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
+#import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
@@ -23,25 +23,25 @@ streamlit.dataframe(my_fruit_list)
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)
-def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
+#streamlit.dataframe(fruits_to_show)
+#def get_fruityvice_data(this_fruit_choice):
+  #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+  #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+ # return fruityvice_normalized
   
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("please select a fruit")
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
+#streamlit.header("Fruityvice Fruit Advice!")
+#try:
+ # fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ # if not fruit_choice:
+ #   streamlit.error("please select a fruit")
+ # else:
+ #   back_from_function = get_fruityvice_data(fruit_choice)
+  #  streamlit.dataframe(back_from_function)
     
     
-except URLError as e:
-  streamlit.error()
+#except URLError as e:
+ # streamlit.error()
 #import requests
 
 #streamlit.text(fruityvice_response.json())
@@ -51,15 +51,15 @@ except URLError as e:
 # write your own comment - what does this do?
 
 
-def get_fruit_load_list():
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from fruit_load_list")
-    return my_cur.fetchall()
-if streamlit.button('Get Fruit Load List'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_data_rows = get_fruit_load_list()
-  my_cnx.close()
-  streamlit.dataframe(my_data_rows)
+#def get_fruit_load_list():
+ # with my_cnx.cursor() as my_cur:
+  #  my_cur.execute("select * from fruit_load_list")
+  #  return my_cur.fetchall()
+#if streamlit.button('Get Fruit Load List'):
+ #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+ # my_data_rows = get_fruit_load_list()
+ # my_cnx.close()
+ # streamlit.dataframe(my_data_rows)
 
 #streamlit.stop()
 #import snowflake.connector
